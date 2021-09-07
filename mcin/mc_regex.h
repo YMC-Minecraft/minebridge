4fda67baf291b47a9d5c5e7e1dcdeaebdc3ec8b2
Trumeet <yuuta@yuuta.moe>
2021-09-07 11:38:07 -0700

    fix: add ^ to match the whole line

--- mcin/mc_regex.h
+++ mcin/mc_regex.h
@@ -1,57 +1,57 @@
 #ifndef _MC_REGEX_H
 #define _MC_REGEX_H
 #define MC_REGEX \
-".* joined the game," \
-".* lost connection: .*," \
-"Stopping server," \
-"Starting minecraft server version .*," \
-"Done \\(.*s\\)! For help type \"help\"," \
-"There are .* of a max of .* players online:.*," \
-".* has made the advancement \\[.*\\]," \
-".* has completed the challenge \\[.*\\]," \
-".* has reached the goal \\[.*\\]," \
-".* was shot by .*," \
-".* was pummeled by .*," \
-".* was pricked to death," \
-".* walked into a cactus whilst trying to escape .*," \
-".* drowned.*," \
-".* experienced kinetic energy.*," \
-".* blew up," \
-".* was blown up by .*," \
-".* was killed by \\[Intentional Game Design\\]," \
-".* hit the ground too hard.*," \
-".* fell from a high place," \
-".* fell off.*," \
-".* fell while.*," \
-".* was squashed by a falling .*," \
-".* went up in flames," \
-".* walked into fire whilst fighting .*," \
-".* burned to death," \
-".* was burnt to a crisp whilst fighting .*," \
-".* went off with a bang.*," \
-".* tried to swim in lava.*," \
-".* was struck by lightning.*," \
-".* discovered the floor was lava," \
-".* walked into danger zone due to .*," \
-".* was killed by magic.*," \
-".* was killed by .* using .*," \
-".* froze to death.*," \
-".* was slain by .*," \
-".* was fireballed by .*," \
-".* was stung to death," \
-".* was shot by a skull from .*," \
-".* starved to death.*," \
-".* suffocated in a wall.*," \
-".* was squished too much," \
-".* was squashed by .*," \
-".* was poked to death by a sweet berry bush.*," \
-".* was killed trying to hurt .*," \
-".* was killed by .* trying to hurt .*," \
-".* was impaled by .*," \
-".* fell out of the world," \
-".* didn't want to live in the same world as.*," \
-".* withered away.*," \
-"<.*> .*"
+"^.* joined the game," \
+"^.* lost connection: .*," \
+"^Stopping server," \
+"^Starting minecraft server version .*," \
+"^Done \\(.*s\\)! For help type \"help\"," \
+"^There are .* of a max of .* players online:.*," \
+"^.* has made the advancement \\[.*\\]," \
+"^.* has completed the challenge \\[.*\\]," \
+"^.* has reached the goal \\[.*\\]," \
+"^.* was shot by .*," \
+"^.* was pummeled by .*," \
+"^.* was pricked to death," \
+"^.* walked into a cactus whilst trying to escape .*," \
+"^.* drowned.*," \
+"^.* experienced kinetic energy.*," \
+"^.* blew up," \
+"^.* was blown up by .*," \
+"^.* was killed by \\[Intentional Game Design\\]," \
+"^.* hit the ground too hard.*," \
+"^.* fell from a high place," \
+"^.* fell off.*," \
+"^.* fell while.*," \
+"^.* was squashed by a falling .*," \
+"^.* went up in flames," \
+"^.* walked into fire whilst fighting .*," \
+"^.* burned to death," \
+"^.* was burnt to a crisp whilst fighting .*," \
+"^.* went off with a bang.*," \
+"^.* tried to swim in lava.*," \
+"^.* was struck by lightning.*," \
+"^.* discovered the floor was lava," \
+"^.* walked into danger zone due to .*," \
+"^.* was killed by magic.*," \
+"^.* was killed by .* using .*," \
+"^.* froze to death.*," \
+"^.* was slain by .*," \
+"^.* was fireballed by .*," \
+"^.* was stung to death," \
+"^.* was shot by a skull from .*," \
+"^.* starved to death.*," \
+"^.* suffocated in a wall.*," \
+"^.* was squished too much," \
+"^.* was squashed by .*," \
+"^.* was poked to death by a sweet berry bush.*," \
+"^.* was killed trying to hurt .*," \
+"^.* was killed by .* trying to hurt .*," \
+"^.* was impaled by .*," \
+"^.* fell out of the world," \
+"^.* didn't want to live in the same world as.*," \
+"^.* withered away.*," \
+"^<.*> .*"
 #define MC_REGEX_WARN \
-"Can't keep up! Is the server overloaded\\? Running .*ms or .* ticks behind"
+"^Can't keep up! Is the server overloaded\\? Running .*ms or .* ticks behind"
 #endif // _MC_REGEX_H
